07b4654a4fe4075c73253bcdd42a387db3994b92
Corbin Crutchley <git@crutchcorn.dev>
2024-03-17 11:37:53 -0600

    feat: add initial implementation of linked fields

--- packages/form-core/src/utils.ts
+++ packages/form-core/src/utils.ts
@@ -223,7 +223,7 @@ interface SyncValidatorArrayPartialOptions<T> {
   validators?: T
 }
 
-interface SyncValidator<T> {
+export interface SyncValidator<T> {
   cause: ValidationCause
   validate: T
 }
